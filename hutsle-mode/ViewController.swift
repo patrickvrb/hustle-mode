ba67661406c49005cc4cc244d25cf06ff672c944
Patrick Beal <patrickvrb@gmail.com>
2018-12-30 14:58:04 -0200

    Initial Commit

---
+++ hutsle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hutsle-mode
+//
+//  Created by Patrick Beal on 30/12/18.
+//  Copyright © 2018 Patrick Beal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
